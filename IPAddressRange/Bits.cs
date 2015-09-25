63f06967b76eb3779bd0e94f9bd4f5e2fbf97803
Corax <aeoneluminaty2@gmail.com>
2015-09-25 18:50:41 +0300

    remove Memory expensive Linq operations with bytes

--- IPAddressRange/Bits.cs
+++ IPAddressRange/Bits.cs
@@ -9,17 +9,39 @@ namespace NetTools
     {
         public static byte[] Not(byte[] bytes)
         {
-            return bytes.Select(b => (byte)~b).ToArray();
+			byte[] res = new byte[bytes.Length];
+
+			for (var i = 0; i < bytes.Length; i++)
+				res[i] = (byte)~bytes[i];
+
+			return res;
+         //   return bytes.Select(b => (byte)~b).ToArray();
         }
 
         public static byte[] And(byte[] A, byte[] B)
         {
-            return A.Zip(B, (a, b) => (byte)(a & b)).ToArray();
+			int resLength = Math.Min(A.Length, B.Length);
+
+			byte[] res = new byte[resLength];
+			
+			for(var i = 0; i < resLength; i++)
+				res[i] = (byte)(A[i] & B[i]);
+
+			return res;
+         //   return A.Zip(B, (a, b) => (byte)(a & b)).ToArray();
         }
 
         public static byte[] Or(byte[] A, byte[] B)
         {
-            return A.Zip(B, (a, b) => (byte)(a | b)).ToArray();
+			int resLength = Math.Min(A.Length, B.Length);
+
+			byte[] res = new byte[resLength];
+
+			for (var i = 0; i < resLength; i++)
+				res[i] = (byte)(A[i] | B[i]);
+
+			return res;
+	//		return A.Zip(B, (a, b) => (byte)(a | b)).ToArray();
         }
 
         public static bool GE(byte[] A, byte[] B)
@@ -45,7 +67,17 @@ namespace NetTools
             {
                 maskBytes[i] = 0xff;
             }
-            if (bitsLen > 0) maskBytes[bytesLen] = (byte)~Enumerable.Range(1, 8 - bitsLen).Select(n => 1 << n - 1).Aggregate((a, b) => a | b);
+			if (bitsLen > 0)
+			{
+				int aggregate = 0;
+
+				for (int i  = 1; i <= 8 - bitsLen; i++)
+					aggregate = (1 << i - 1) | aggregate; 
+
+				maskBytes[bytesLen] = (byte)~aggregate;				
+		//		maskBytes[bytesLen] = (byte)~Enumerable.Range(1, 8 - bitsLen).Select(n => 1 << n - 1).Aggregate((a, b) => a | b);
+			}
+
             return maskBytes;
         }
 
